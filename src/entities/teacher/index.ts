28d0f2a1766ea5b9345830b55c30eef64aad979a
yeegie <lotus9200@gmail.com>
2024-06-22 13:17:19 +0600

    feat: added types

---
+++ src/entities/teacher/index.ts
@@ -0,0 +1,2 @@
+export { TeacherCard } from "./ui/teacherCard";
+export { type ITeacher } from "./model/types";
---
+++ src/entities/teacher/model/types.ts
@@ -0,0 +1,7 @@
+export interface ITeacher {
+  image: string;
+  name: string;
+  bio: string;
+  skills: string;
+  reviews: number;
+}
