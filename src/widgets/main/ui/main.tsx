4740b305cf5298a7a4fe6e1b01ffd076e907b2e3
yeegie <lotus9200@gmail.com>
2024-06-22 13:20:51 +0600

    chore: update content

--- src/widgets/main/ui/main.tsx
+++ src/widgets/main/ui/main.tsx
@@ -3,18 +3,25 @@ import clsx from "clsx";
 
 import { Title } from "@shared/ui/title";
 import { Paragraph } from "@shared/ui/paragraph";
-
-import { CourseCard } from "@shared/ui/course-card";
+import { Button } from "@shared/ui/button";
 
 export const Main: React.FC = () => {
-  return <div className={clsx(styles.main)}>
-    <div>
-      <Title size={2}>Программирование для детей</Title>
-      <Paragraph>Развитие в ребёнке креативности и поиске не стандартных решений</Paragraph>
+  return (
+    <div className={clsx(styles.main)}>
+      <div className={styles.content}>
+        <Title fontSize="25pt">Программирование для детей</Title>
+        <Paragraph>
+          Развитие в ребёнке креативности и поиске нестандартных решений
+        </Paragraph>
+        <div>
+          <Button text="Python" variant="fill" />
+          <Button text="Python" variant="fill" />
+          <Button text="Python" variant="fill" />
+          <Button text="Python" variant="fill" />
+          <Button text="Python" variant="fill" />
+          <Button text="Python" variant="fill" />
+        </div>
+      </div>
     </div>
-    <div>
-
-    </div>
-  </div>
+  );
 };
-
